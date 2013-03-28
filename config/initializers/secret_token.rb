35293707ebe9ba35fa529ea70cf336937143ad27
Gary Riger <grigerny@aol.com>
2013-03-27 21:58:26 -0400

    second commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nabehood::Application.config.secret_token = 'a872a11338d8df315f82e560b0f3d7f8c49399b82274f3880d45d3092f2cade1452d899a350af80a6ba24fbd99ebf8024520ba754a9a54de540a22fcc8059ee3'
