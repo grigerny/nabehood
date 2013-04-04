c03801f577ae9e743dd425effeed8776f0df31b2
Gary Riger <grigerny@aol.com>
2013-04-03 21:58:18 -0400

    added username to omniauth

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130404014256) do
+ActiveRecord::Schema.define(:version => 20130404014852) do
 
   create_table "cities", :force => true do |t|
     t.datetime "created_at", :null => false
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(:version => 20130404014256) do
     t.datetime "updated_at",                             :null => false
     t.string   "provider"
     t.string   "uid"
+    t.string   "username"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
